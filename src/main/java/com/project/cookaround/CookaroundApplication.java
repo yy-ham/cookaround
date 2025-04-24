ce1977b90ecd9f2f4aa064836e550f7fdb187e8c
박예진 <yy6037@naver.com>
2025-04-24 15:05:45 +0900

    First Commit

---
+++ src/main/java/com/project/cookaround/CookaroundApplication.java
@@ -0,0 +1,13 @@
+package com.project.cookaround;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookaroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookaroundApplication.class, args);
+	}
+
+}
