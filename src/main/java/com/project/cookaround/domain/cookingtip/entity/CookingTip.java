80f5272ae0183f28da2c3348a5fbe94ad9f5a435
yy-ham <yyham.dev@gmail.com>
2025-07-23 22:11:53 +0900

    feat: 요리팁 엔티티 및 카테고리 Enum 추가
    #10

---
+++ src/main/java/com/project/cookaround/domain/cookingtip/entity/CookingTip.java
@@ -0,0 +1,59 @@
+package com.project.cookaround.domain.cookingtip.entity;
+
+import com.project.cookaround.domain.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+@Table(name = "cooking_tip")
+public class CookingTip {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_cooking_tip")
+    @SequenceGenerator(name = "seq_cooking_tip", sequenceName = "seq_cooking_tip", allocationSize = 1)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Enumerated(EnumType.STRING)
+    private CookingTipCategory category;
+
+    private String title;
+    private String description;
+    private String content;
+
+    @Column(name = "view_count")
+    private Long viewCount;
+
+    @Column(name = "like_count")
+    private Long likeCount;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+
+    @PrePersist
+    public void prePersist() {
+        LocalDateTime now = LocalDateTime.now();
+        this.createdAt = now;
+        this.updatedAt = now;
+
+        this.viewCount = 0L;
+        this.likeCount = 0L;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+}
---
+++ src/main/java/com/project/cookaround/domain/cookingtip/entity/CookingTipCategory.java
@@ -0,0 +1,9 @@
+package com.project.cookaround.domain.cookingtip.entity;
+
+public enum CookingTipCategory {
+
+    PREPARATION,            // 손질법
+    STORAGE,                // 보관법
+    COOKING                 // 조리법
+
+}
