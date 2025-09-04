107f97387e4ad60e6e339a6d0ebaddad46eb4c52
yy-ham <yyham.dev@gmail.com>
2025-09-04 13:23:18 +0900

    feat: 요리팁 상세 조회 시 카테고리명을 영어에서 UI 표시용 한글로 수정
    #10

--- src/main/java/com/project/cookaround/domain/cookingtip/entity/CookingTipCategory.java
+++ src/main/java/com/project/cookaround/domain/cookingtip/entity/CookingTipCategory.java
@@ -2,8 +2,18 @@ package com.project.cookaround.domain.cookingtip.entity;
 
 public enum CookingTipCategory {
 
-    PREPARATION,            // 손질법
-    STORAGE,                // 보관법
-    COOKING                 // 조리법
+    PREPARATION("손질법"),
+    STORAGE("보관법"),
+    COOKING("조리법")
+    ;
 
+    private String description;
+
+    CookingTipCategory(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
